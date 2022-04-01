05de8149243e0b521f475d2891750ad05003fb01
Daniel Aguila <daguila.rojas03@gmail.com>
2022-03-31 18:23:45 -0600

    Commit inicial

---
+++ Arreglos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string[] friends = new string[7];
+            string[] friends = new string[7] {
+                "Antonio",
+                "Rosario",
+                "Maria",
+                "Taco",
+                null,
+                null,
+                null
+            };
+
+
+            // friends[0] = "Antonio";
+            Console.WriteLine(friends[1]);
+        }
+    }
+}
